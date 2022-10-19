f1ff931816d098cbdf63bdb97ce8d64d3e06b222
0xjoshva <joshuasteed123@gmail.com>
2022-10-19 19:33:30 +0200

    things are working

--- src/components/AboutEducation.vue
+++ src/components/AboutEducation.vue
@@ -1,16 +1,63 @@
 <template>
   <section>
     <div class="container">
-    <h1 id="header">Education</h1>
-    <div class="wrapper">
+      <h1 id="header">Education</h1>
+      <div class="wrapper">
         <div class="icontainer">
-        <div class="item"></div>
-        <div class="item"></div>
+          <div class="item">
+            <div class="front card-content">
+              <div class="head">
+                <img src="../assets/designcourse101.png" alt="" />
+                <p class="title">DesignCourse</p>
+              </div>
+              <div class="details">
+                <p class="course">UI/UX Design Course</p>
+                <p class="date">Oct 2022</p>
+              </div>
+            </div>
+            <div class="back card-content">I did a UX DESIGN COURSE</div>
+          </div>
+          <div class="item">
+            <div class="front card-content">
+              <div class="head">
+                <img src="../assets/camst.png" alt="" />
+                <p class="title">Cape Academy</p>
+              </div>
+              <div class="details">
+                <p class="course">Matric Certificate (NSC)</p>
+                <p class="date">Jan 2017 - Dec 2021</p>
+              </div>
+            </div>
+            <div class="back card-content">I did a UX DESIGN COURSE</div>
+          </div>
         </div>
-        <div class="item"></div>
-        <div class="item"></div>
-    </div>
-
+        <div class="item">
+          <div class="front card-content">
+            <div class="head">
+              <img src="../assets/lifechoices.png" alt="" />
+              <p class="title">Life Choices Academy</p>
+            </div>
+            <div class="details">
+              <p class="course">UI/UX Design Course</p>
+              <p class="date">Sep 2022</p>
+            </div>
+          </div>
+          <div class="back card-content">I did a UX DESIGN COURSE</div>
+        </div>
+        <div class="item">
+          <div class="front card-content">
+            <div class="head">
+              <img src="../assets/aws.png" alt="" />
+              <p class="title">Amazon Web Services</p>
+            </div>
+            <div class="details">
+              <p class="course">Cloud Practitioner</p>
+              <p class="date">Coming soon...</p>
+            </div>
+          </div>
+          <div class="back card-content">I did a UX DESIGN COURSE</div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -18,14 +65,14 @@
 export default {};
 </script>
 <style scoped>
-section{
-    height: fit-content;
-    width: 100%;
-    padding-bottom: 2rem;
+section {
+  height: fit-content;
+  width: 100%;
+  padding-bottom: 2rem;
 }
-.container{
-    width: 40%;
-    margin: 0 auto;
+.container {
+  width: 40%;
+  margin: 0 auto;
 }
 #header {
   font-family: "Poppins";
@@ -34,27 +81,99 @@ section{
   font-size: 26px;
   color: white;
 }
-.wrapper{
-    margin-top: 1rem;
-    display: flex;
-    column-gap: 1rem;
+.wrapper {
+  margin-top: 1rem;
+  display: flex;
+  column-gap: 1rem;
 }
-.item{
-    width: 266px;
-    height: 183px;
-    background: var(--panel);
+.item {
+  width: 242px;
+  height: 183px;
 }
-.item:nth-child(1){
-    border-top-left-radius: 12px;
+.item:nth-child(1) {
+  border-top-left-radius: 12px;
 }
-.icontainer{
-    display: flex;
-    flex-direction: column;
-    row-gap: 1rem;
-    
+.icontainer {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
 }
-.icontainer > div:nth-child(2){
-    border-bottom-left-radius: 12px;
+.icontainer > div:nth-child(2) {
+  border-bottom-left-radius: 12px;
 }
 
+.item {
+  position: relative;
+  perspective: 1000px;
+}
+.front {
+  background: rgb(46 46 72);
+  width: inherit;
+  height: inherit;
+  transition: 0.5s;
+  position: absolute;
+  z-index: 1;
+  padding: 1rem;
+}
+.back {
+  background: rgb(40, 40, 71);
+  width: inherit;
+  height: inherit;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: rotateY(180deg);
+}
+.item:hover .front {
+  transform: rotateY(180deg);
+  opacity: 0;
+}
+.item:hover .back {
+  transform: rotateY(0);
+}
+.title {
+  font-weight: 500;
+  font-size: 18px;
+  font-family: "Poppins";
+  color: white;
+  padding-left: 1rem;
+}
+.head {
+  display: flex;
+  width: fit-content;
+  align-items: center;
+}
+.course {
+  font-family: "Poppins";
+  color: white;
+  font-size: 1rem;
+  font-weight: 300;
+}
+.date {
+  font-family: "Poppins";
+  font-weight: 400;
+  color: var(--text);
+  font-size: 12px;
+  display: flex;
+  justify-self: end;
+}
+.details {
+  padding-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  min-height: 5rem;
+  height: fit-content;
+  justify-content: space-between;
+}
+.card-content {
+  /* box-shadow: 0 2rem 2rem rgba(0, 0, 0, 0.281); */
+  transition: all 0.5s;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+}
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="heading">
-  <h1>My projects</h1>
-  <p>
-    I picked out projects I am most proud of <br />
-    and enjoyed the most
-  </p>
+    <h1>My projects</h1>
+    <p>
+      Carefully picked projects, 
+      emphasizing <br /> the ones I enjoyed the most
+    </p>
   </div>
   <ProjectComponent />
 </template>
@@ -17,20 +17,21 @@ export default {
 };
 </script>
 <style scoped>
-.heading{
-    width: 40%;
-    margin: 0 auto;
-    padding-top: 5rem;
+.heading {
+  width: 40%;
+  margin: 0 auto;
+  padding-top: 5rem;
 }
-.heading h1{
- color: white;
+.heading h1 {
+  color: white;
   font-family: "Poppins";
   font-size: 36px;
   font-weight: 600;
 }
-.heading p{
+.heading p {
   padding-top: 1.3rem;
   color: var(--text);
   font-family: "Poppins";
 }
+
 </style>
